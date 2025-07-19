a8c923028e8230c92de9af3163f4f63d09c77d6c
Lucas Ramalho <adryelll85@gmail.com>
2025-07-19 00:10:34 -0300

    add: templates

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
-def home_page():
-    return render_template('home.html')
+def index_page():
+    return render_template('index.html')
 
