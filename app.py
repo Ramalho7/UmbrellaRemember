17b5dd613ced2ec8bf11d9c7888adc5b589a8cfd
Lucas Ramalho <adryelll85@gmail.com>
2025-07-23 20:36:30 -0300

    Update .gitignore and add initial model structure with environment configuration

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 from flask import Flask, render_template, request
 app=Flask(__name__)
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_NAME = os.getenv('DB_NAME')
+
+engine = create_engine(f'mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}')
 
 @app.route('/', methods=["GET","POST"])
 def index_page():
---
+++ models/model.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine, Column, Integer, String, text
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+import os
+
+def registerUser():
+    
+    return
